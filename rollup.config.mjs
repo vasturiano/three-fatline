e35941fbe1f78f879960fae9657011ff512a0d4e
vasco <vastur@gmail.com>
2022-12-27 00:09:40 +0000

    Bump deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,9 @@ import commonJs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import { terser } from "rollup-plugin-terser";
 import dts from 'rollup-plugin-dts';
-import { name, homepage, version, dependencies, peerDependencies } from './package.json';
+
+import pkg from './package.json' assert { type: 'json' };
+const { name, homepage, version, dependencies, peerDependencies } = pkg;
 
 const umdConf = {
   format: 'umd',
